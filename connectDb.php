2d3a9aba2711b75b71b6ce5bc91cbd34f146ca55
yogesh1078 <162011268+yogesh1078@users.noreply.github.com>
2024-09-07 21:17:38 +0530

    Update connectDb.php

--- connectDb.php
+++ connectDb.php
@@ -2,7 +2,7 @@
    
     $severname = "localhost";
     $username  =" yogesh-1078";
-    $password  ="8078669365";
+    $password  ="........";
     try {
     $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
